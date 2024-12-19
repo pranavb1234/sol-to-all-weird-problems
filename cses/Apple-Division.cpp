49a93194b4f6d91a99a87aea2ad4b7132754054e
pranavb1234 <pranavtech@rediffmail.com>
2024-12-19 23:56:13 +0530

    New files added testing a bash script

--- cses/Apple-Division.cpp
+++ cses/Apple-Division.cpp
@@ -10,27 +10,40 @@ typedef long long ll;
 
 signed main(){
 	
-	string s;
-	cin >> s;
-	vector<string> vs;
-	for (int i = 0; i < s.size(); ++i){
-		string temp = " ";
-		temp = temp + s[i];
-		for (int j = 0; j < s.size(); ++j){
-			if(j == i) continue;
-			 else{
-			 	
-			 }
+	ll n;
+	cin >> n;
+	vector<ll> v(n);
+	vector<ll> ans;
+	ll sum = 0;
+	for (int i = 0; i < n; ++i)
+	{
+		cin >> v[i];
+		sum += v[i];
+	}
 
+	auto temp = v;
+	sort(temp.begin(), temp.end());
+
+	ll gOne = temp[0] + temp[n-1];
+	ll gSec = sum - gOne;
+
+	ll diff = abs(gOne - gSec);
+	ans.push_back(diff);
+
+	for (int i = 1; i < n-1; ++i)
+	{
+		gOne += temp[i];
+		gSec = sum - gOne;
+		diff = abs(gOne - gSec);
+
+		if(diff < ans[ans.size() - 1]){
+			ans.push_back(diff);
+		}else {
+			continue;
 		}
-
-		vs.push_back(temp);
 	}
 
-	cout << vs.size() << '\n';
-	for(auto &a : vs){
-		cout << a << '\n';
-	}
+	cout << ans[ans.size() - 1] << endl;
 
 }
 
---
+++ cses/playlist.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+signed main() {
+    ll n;
+    cin >> n;
+    vector<ll> v(n);
+    for (ll i = 0; i < n; i++) {
+        cin >> v[i];
+    }
+
+    set<ll> s; 
+    vector<ll> ans;
+    int start = 0;
+    
+
+    for (int end = 0; end < n; ++end) {
+        while (s.find(v[end]) != s.end()) {
+        	s.erase(v[start]);
+            start++;
+        }
+        s.insert(v[end]);
+    
+        ans.push_back(end - start + 1);
+    }
+
+
+    sort(ans.begin(), ans.end());
+
+
+    cout << ans[ans.size()-1] << '\n';
+}
+
