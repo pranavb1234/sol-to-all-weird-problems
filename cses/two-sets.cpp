95aa0ec6b6f306363583c49089ff47fd29f6349c
pranavb1234 <pranavtech@rediffmail.com>
2024-12-19 16:42:25 +0530

    New files added testing a bash script

---
+++ cses/two-sets.cpp
@@ -0,0 +1,24 @@
+/* 
+author: Pranav
+Prob : Two-Sets
+Lang: C++
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+signed main(){
+	ll n;
+	cin >> n;
+	int cal = (n * (n + 1)) / 2;
+	if(cal % 2 != 0){
+		cout << "NO" << endl;
+		return 0;
+	}else{
+		
+
+	}
+
+
+}
